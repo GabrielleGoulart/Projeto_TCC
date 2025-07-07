58aa7f000ae88db0b5650d89237fc5f75c81dcfa
GabrielleGoulart <gabriellegoulart48@gmail.com>
2025-07-07 20:57:04 -0300

    Add files via upload

---
+++ EstoqueController.java
@@ -0,0 +1,12 @@
+package com.projeto_ordemservico.projeto_ordemservico;
+
+import javafx.fxml.FXML;
+
+//codigo feito!
+public class EstoqueController {
+  @FXML private TableWiew TbPeca;
+  @FXML private Button BtVoltar;
+  @FXML private TableWiew<Peca> TabelaPeca;
+  @FXML
+  @FXML
+}
